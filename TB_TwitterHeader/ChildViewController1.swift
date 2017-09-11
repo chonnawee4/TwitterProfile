38d26a313a6ab0395489691621c3d25d2187c091
Chonnawee Chatcherthaicharoen <chonnawee.ch@gmail.com>
2017-09-12 01:54:28 +0700

    Add UITableView

--- TB_TwitterHeader/ChildViewController1.swift
+++ TB_TwitterHeader/ChildViewController1.swift
@@ -9,10 +9,32 @@
 import Foundation
 import XLPagerTabStrip
 
+let cellIdentifier = "cell"
+
 class ChildViewController1: UIViewController, IndicatorInfoProvider {
+
+  @IBOutlet weak var tableView: UITableView!
 
   func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
     return IndicatorInfo(title: "Tweets")
   }
 
+}
+
+extension ChildViewController1: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+
+    cell.textLabel?.text = "Label \(indexPath.row)"
+
+    return cell
+  }
 }
